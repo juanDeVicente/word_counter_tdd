dcd2cca9488cd7dccfbb613a49e33c345be71ac3
raulmartinezL <raulml2527@gmail.com>
2019-03-15 22:11:09 +0100

    Probando esta cosa 2

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -40,7 +40,7 @@ def test_wikipedia_definition_of_test_spanish():
         'de cifrado que, aunque no tienen sentido para cualquier persona, sí puede ser descifrado por su ' \
         'destinatario original. En otras palabras, un texto es un entramado de signos con una intención ' \
         'comunicativa que adquiere sentido en determinado contexto. Las ideas esenciales que comunica un texto ' \
-        'están contenidas en lo que se suele denominar «macroproposicione    s», unidades estructurales de nivel ' \
+        'están contenidas en lo que se suele denominar «macroproposiciones», unidades estructurales de nivel ' \
         'superior o global, que otorgan coherencia al texto constituyendo su hilo central, el esqueleto ' \
         'estructural que cohesiona elementos lingüísticos formales de alto nivel, como los títulos y subtítulos, ' \
         'la secuencia de párrafos, etc. En contraste, las «microproposiciones» son los elementos coadyuvantes de ' \
